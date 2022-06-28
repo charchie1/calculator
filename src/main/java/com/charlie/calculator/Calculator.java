ce72df1fe24f44fee28ab3b2c8a3db32f1324b05
charchie1 <charchiefpl@gmail.com>
2022-06-28 10:15:59 +0100

    Moved duplicate overflow check to method.

--- src/main/java/com/charlie/calculator/Calculator.java
+++ src/main/java/com/charlie/calculator/Calculator.java
@@ -1,24 +1,30 @@
 package com.charlie.calculator;
 
 public class Calculator {
-        public static int add (int num1, int num2 )
-        {
-                int r = num1 + num2;
-                if (((num1 ^ r) & (num2 ^ r)) < 0) {
-                        throw new ArithmeticException("int overflow");
-                }
-                return r;
-        }
-        public static int subtract (int num1, int num2 )
-        {return num1 - num2;}
-        public static int multiply (int num1, int num2 )
-        {
-                int r = num1 * num2;
-                if (((num1 ^ r) & (num2 ^ r)) < 0) {
-                        throw new ArithmeticException("int overflow");
-                }
-                return r;}
-        public static int divide (int num1, int num2 )
-        {return num1 / num2;}
+    public static int add(int num1, int num2) {
+        int r = num1 + num2;
+        checkOverflow(num1, num2, r);
+        return r;
+    }
 
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2) {
+        int r = num1 * num2;
+        checkOverflow(num1, num2, r);
+        return r;
+    }
+
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    private static void checkOverflow(int num1, int num2, int r) {
+        if (((num1 ^ r) & (num2 ^ r)) < 0) {
+            throw new ArithmeticException("int overflow");
         }
+    }
+
+}
